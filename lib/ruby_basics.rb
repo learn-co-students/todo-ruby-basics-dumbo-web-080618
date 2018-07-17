a98813fe19461d8061180d19bed84f76b202df5e
L H <lynn44229@gmail.com>
2018-07-17 22:33:32 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,32 +1,27 @@
 def division(num1, num2)
- return num1/num2
+ num1/num2
 end
-division(36,6)
+
 def assign_variable(value)
-  puts value
+  value
 end
-assign_variable("Bob")
 
-def argue(a)
-  return a
+def argue(str)
+  str
 end
-argue()
 
-def greeting (greeting,name)
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-  return "Nice"
+  "Nice"
 end
-return_a_value
 
 def last_evaluated_value
-  puts "expert"
+  "expert"
 end
-last_evaluated_value
 
-def pizza_party
-  puts "cheese"
+def pizza_party(top="cheese")
+  top
 end
-pizza_party
